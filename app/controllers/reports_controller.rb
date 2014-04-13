f644e39b9893c9b1230b95c7db0c1d68b98250f1
Abhishek Srivastava <abhishek77in@gmail.com>
2014-04-13 20:28:08 +0530

    Create report for whole abdomen

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,13 +1,15 @@
 class ReportsController < ApplicationController
   def new
     @report = Report.new
-    @report.report_type = ReportType.new(reportable: WholeAbdomen.new)
+    # @report.report_type = ReportType.new(reportable: WholeAbdomen.new)
   end
 
   def create
     @report = Report.new(report_params)
-    if @report.save
-      redirect_to '/', notice: 'Report Created'
+    @report.report_type = ReportType.new(reportable: WholeAbdomen.new(report_type_attributes))
+    if @report.save && @report.report_type.save
+      raise
+      redirect_to new_reports_path, notice: 'Report Created'
     else
       render 'new'
     end
@@ -16,5 +18,9 @@ class ReportsController < ApplicationController
   private
   def report_params
     params.require(:report).permit(:doctor_id, patient_attributes: [:first_name, :last_name, :title, :age, :sex])
+  end
+
+  def report_type_attributes
+    params.require(:report).require(:report_type_attributes).permit!
   end
 end
--- app/models/report_type.rb
+++ app/models/report_type.rb
@@ -1,4 +1,5 @@
 class ReportType < ActiveRecord::Base
   belongs_to :report
   belongs_to :reportable, polymorphic: true
+  accepts_nested_attributes_for :reportable, allow_destroy: true
 end
