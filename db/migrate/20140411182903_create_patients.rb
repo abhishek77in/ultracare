c0fcec7153c07ffcf06216876e3e665cec27c04a
Abhishek Srivastava <abhishek77in@gmail.com>
2014-04-13 18:07:54 +0530

    Keep on name field on patients

--- db/migrate/20140411182903_create_patients.rb
+++ db/migrate/20140411182903_create_patients.rb
@@ -1,9 +1,7 @@
 class CreatePatients < ActiveRecord::Migration
   def change
     create_table :patients do |t|
-      t.string :title
-      t.string :first_name
-      t.string :last_name
+      t.string :name
       t.integer :age
       t.string :sex
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140411184340) do
+ActiveRecord::Schema.define(version: 20140413123131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,9 +24,7 @@ ActiveRecord::Schema.define(version: 20140411184340) do
   end
 
   create_table "patients", force: true do |t|
-    t.string   "title"
-    t.string   "first_name"
-    t.string   "last_name"
+    t.string   "name"
     t.integer  "age"
     t.string   "sex"
     t.datetime "created_at"
