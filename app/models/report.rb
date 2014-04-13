0f17e20b5bb5b47a89e18f3e8da118c1af838ccd
Abhishek Srivastava <abhishek77in@gmail.com>
2014-04-13 16:16:33 +0530

    Add patient form

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,7 @@
 class Report < ActiveRecord::Base
   belongs_to :doctor
   belongs_to :patient
+  accepts_nested_attributes_for :patient
   has_one :report_type
+  accepts_nested_attributes_for :report_type, allow_destroy: true
 end
