9cbf0b7a6421310e6e7533e5125b5cbfa1ac02ff
Abhishek Srivastava <abhishek77in@gmail.com>
2014-04-13 16:37:12 +0530

    Accept patient params

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,5 +5,16 @@ class ReportsController < ApplicationController
   end
 
   def create
+    @report = Report.new(report_params)
+    if @report.save
+      redirect_to '/', notice: 'Report Created'
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def report_params
+    params.require(:report).permit(:doctor_id, patient_attributes: [:first_name, :last_name, :title, :age, :sex])
   end
 end
