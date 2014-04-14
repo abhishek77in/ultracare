a2dd2d6ad2fb7df5a89b292b196d8d339d840b86
Abhishek Srivastava <abhishek77in@gmail.com>
2014-04-15 02:48:48 +0530

    Redirect to root after creating the report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -9,7 +9,7 @@ class ReportsController < ApplicationController
     @report = Report.new(report_params)
     @report.report_type = ReportType.new(reportable: WholeAbdomen.new(report_type_attributes))
     if @report.save && @report.report_type.save
-      redirect_to new_reports_path, notice: 'Report Created'
+      redirect_to root_path, notice: 'Report Created'
     else
       flash[:alert] = "Sorry! Report could not be created, please fix the errors and try again."
       render 'new'
