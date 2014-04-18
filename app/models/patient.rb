7800af73b1044c9164244e175d070b06ea317192
Abhishek Srivastava <abhishek77in@gmail.com>
2014-04-19 00:58:31 +0530

    Remove validation on age

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,4 @@
 class Patient < ActiveRecord::Base
   has_many :reports
   validates_presence_of :name
-  validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than: 120 }
 end
