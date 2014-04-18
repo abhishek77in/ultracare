37ee78dd1dafb6387df7125e5ccb3a31795401fb
Abhishek Srivastava <abhishek77in@gmail.com>
2014-04-19 00:32:47 +0530

    Allow adding doctors in app

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Ultrasound::Application.routes.draw do
   resources "reports" do
     get 'print', on: :member
   end
+  resources :doctors
   root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
