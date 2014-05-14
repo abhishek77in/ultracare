88e908b8ba4f18873c1beb3bb9b28516695dd3ee
Abhishek Srivastava <abhishek77in@gmail.com>
2014-05-14 23:26:49 +0530

    Allow adding doctors quickly

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,4 +2,8 @@ class Doctor < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name
   has_many :reports
+
+  def doctor_name
+    "#{name} (#{degree}), # #{id}"
+  end
 end
