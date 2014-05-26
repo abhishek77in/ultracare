e07d46f61b3cf436dbc29db15bea14b89ff1ccfe
Abhishek Srivastava <abhishek77in@gmail.com>
2014-05-26 23:24:17 +0530

    Refactor

--- app/models/report.rb
+++ app/models/report.rb
@@ -8,6 +8,18 @@ class Report < ActiveRecord::Base
   validates_presence_of :doctor, :patient, :report_type
   scope :recent, -> { order('created_at DESC') }
 
+
+  def possible_genders
+    if for_female?
+      [['Female', 'F']]
+    elsif for_male?
+      [['Male', 'M']]
+    else
+      [['Female', 'F'],['Male', 'M']]
+    end
+  end
+
+  private
   def for_male?
     ['Testis','WholeAbdomenMale','KubMale'].include?(report_type.reportable_type)
   end
@@ -15,4 +27,5 @@ class Report < ActiveRecord::Base
   def for_female?
     ['Breast','Obstetric','KubFemale','WholeAbdomenFemale','LowerAbdomenFemale'].include?(report_type.reportable_type)
   end
+
 end
