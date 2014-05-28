6a684830a6c2c7d21ad822ff0f3cda0fd022603e
Abhishek Srivastava <abhishek77in@gmail.com>
2014-05-28 16:20:58 +0530

    Remove new, show for doctors

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,25 +1,11 @@
 class DoctorsController < ApplicationController
   before_action :set_doctor, only: [:show]
 
-  # GET /doctors
-  # GET /doctors.json
   def index
     @doctors = Doctor.recent.paginate(:page => params[:page])
     @doctor = Doctor.new
   end
 
-  # GET /doctors/1
-  # GET /doctors/1.json
-  def show
-  end
-
-  # GET /doctors/new
-  def new
-    @doctor = Doctor.new
-  end
-
-  # POST /doctors
-  # POST /doctors.json
   def create
     @doctor = Doctor.new(doctor_params)
     if @doctor.save
@@ -32,12 +18,10 @@ class DoctorsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_doctor
       @doctor = Doctor.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def doctor_params
       params.require(:doctor).permit(:name, :degree)
     end
