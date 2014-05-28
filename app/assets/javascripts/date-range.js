9b5186f124d1fc3401b1e72993c2772028c5f3d6
Abhishek Srivastava <abhishek77in@gmail.com>
2014-05-28 17:00:51 +0530

    Cutomize date range for business report

--- app/assets/javascripts/date-range.js
+++ app/assets/javascripts/date-range.js
@@ -8,6 +8,20 @@ $(function()
       }
     });
 
+    $('#business-report-date-range').dateRangePicker(
+      {
+      format: 'D-M-YYYY',
+      startOfWeek: 'monday',
+      autoClose: true,
+      shortcuts :
+        {
+        'prev-days': [1,3,7],
+        'prev': ['week','month'],
+        'next-days':null,
+        'next':null
+      }
+    });
+
     $('#date-range').dateRangePicker(
       {
       format: 'D-M-YYYY',
@@ -15,7 +29,7 @@ $(function()
       autoClose: true,
       shortcuts :
         {
-        'prev-days': [1,3,5,7],
+        'prev-days': [1,3,7],
         'prev': ['week','month','year'],
         'next-days':null,
         'next':null
