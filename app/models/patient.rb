8d91fc3067bc7fa24ba4b2230a92c66b191f3a2d
Abhishek Srivastava <abhishek77in@gmail.com>
2014-05-31 09:49:57 +0530

    Add age upper limit

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,5 @@
 class Patient < ActiveRecord::Base
   has_many :reports
   validates_presence_of :name
-  validates :age, presence: true, numericality: { greater_than: 0, less_than: 120 }
+  validates :age, presence: true, numericality: { greater_than: 0, less_than: 100 }
 end
