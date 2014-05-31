3bffe261d994d8eff1ab0cb47f14cc9d6ec2c4ed
Abhishek Srivastava <abhishek77in@gmail.com>
2014-06-01 01:22:02 +0530

    Use configurable print settings

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,5 +1,5 @@
 class Setting < ActiveRecord::Base
-  store :print_settings, accessors: [ :heading, :sub_heading, :address, :phone_number, :consultant_1, :consultant_2], coder: JSON
+  store :print_settings, accessors: [:heading, :sub_heading, :address, :phone_number, :consultant_1, :consultant_2], coder: JSON
   store :default_organ_findings, accessors: [:liver, :gall_bladder, :pancreas, :spleen, :right_kidney,
     :left_kidney, :urinary_bladder, :prostate, :ureter, :adnexa, :uterus, :thyroid_left_lobe, :thyroid_right_lobe,
     :i_j_v_and_c_a, :isthmus, :left_testis, :right_testis, :left_breast, :right_breast, :impression], coder: JSON
