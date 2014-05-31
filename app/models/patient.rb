90b6dd7bafdfabe851ae071a04c7272d2151b3e2
Abhishek Srivastava <abhishek77in@gmail.com>
2014-05-31 09:22:44 +0530

    Add validation on age

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,5 @@
 class Patient < ActiveRecord::Base
   has_many :reports
-  validates_presence_of :name, :age
+  validates_presence_of :name
+  validates :age, presence: true, numericality: { greater_than: 0, less_than: 120 }
 end
