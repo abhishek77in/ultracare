20f058ffdf2baa7ed7445751320a1fe207ab7e2d
Abhishek Srivastava <abhishek77in@gmail.com>
2014-05-31 11:36:07 +0530

    Allow editing doctors

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,5 +1,5 @@
 class DoctorsController < ApplicationController
-  before_action :set_doctor, only: [:show]
+  before_action :set_doctor, only: [:show, :edit, :update]
 
   def index
     @doctors = Doctor.recent.paginate(:page => params[:page])
@@ -12,6 +12,21 @@ class DoctorsController < ApplicationController
       redirect_to doctors_path, notice: 'Doctor was successfully created.'
     else
       flash.now[:alert] = "Sorry! Doctor could not be created, please fix the errors and try again."
+      @doctors = Doctor.recent.paginate(:page => params[:page])
+      render action: 'index'
+    end
+  end
+
+  def edit
+    @doctors = Doctor.recent.paginate(:page => params[:page])
+    render 'index'
+  end
+
+  def update
+    if @doctor.update_attributes(doctor_params)
+      redirect_to doctors_path, notice: 'Doctor details were updated successfully.'
+    else
+      flash.now[:alert] = "Sorry! Doctor details could not be updated, please fix the errors and try again."
       @doctors = Doctor.recent.paginate(:page => params[:page])
       render action: 'index'
     end
