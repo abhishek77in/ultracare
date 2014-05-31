14cfce03ebb9e449146285bb07cedf23230ffbcf
Abhishek Srivastava <abhishek77in@gmail.com>
2014-05-31 23:40:04 +0530

    Fix typo

--- app/models/thyroid_gland.rb
+++ app/models/thyroid_gland.rb
@@ -5,7 +5,6 @@ class ThyroidGland < ActiveRecord::Base
     {
       left_lobe: 'Appears NORMAL in size, shape & echotexture. No focal lesion seen.',
       right_lobe: 'Appears NORMAL in size, shape & echotexture. No focal lesion seen.',
-      impression: 'MINIMAL FLUID COLLECTION SEEN IN RIGHT TESTIS.',
       i_j_v_and_c_a: 'NORMAL',
       isthmus: 'NORMAL',
       impression: 'ORGANS ARE UNDER SCAN WITH IN NORMAL LIMITS.'
