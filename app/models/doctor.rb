b7217dc9fe8d87378e1843b3a34d50101f359a64
Abhishek Srivastava <abhishek77in@gmail.com>
2014-06-01 12:59:19 +0530

    Record updates on doctor name

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,6 @@
 class Doctor < ActiveRecord::Base
+  has_paper_trail on: [:update], only: [:name]
+
   validates_presence_of :name
   validates_uniqueness_of :name, :case_sensitive => false
   has_many :reports
