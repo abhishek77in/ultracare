a8e957ee635b55a17b6e9e2868dc037bded52c46
Abhishek Srivastava <abhishek77in@gmail.com>
2014-06-01 12:00:23 +0530

    Use default obstetric findings

--- app/models/obstetric.rb
+++ app/models/obstetric.rb
@@ -6,22 +6,6 @@ class Obstetric < ActiveRecord::Base
   end
 
   def self.params
-    {
-      pregnancy: "Single\nConfirmed / Viable / Intrauterine",
-      average_g_a: '29 WEEKS O5 DAYS (+- 02 WEEKS)',
-      presentation_and_lie: 'VERTEX',
-      liquor_volume: 'Appears ADEQUATE in amount.',
-      cervix_and_o_s: 'Appears Closed.',
-      fetal_heart: "PULSATING\nRate by M+mode 158/bpm.",
-      fetal_head_movement: 'NORMAL',
-      fetal_limbs_movement: 'NORMAL',
-      fetal_breathing_movement: 'NORMAL',
-      placenta: 'ANTERIOR',
-      grade: '2nd',
-      e_d_d: '06-06-2014(+- 02 WEEKS)',
-      e_f_w: '1430 gms. (+-20%)',
-      remarks: 'Appears Single Viable Intrauterine Fetus with Normal Cardiac & Somatic Activity. Fetal Heart Pulsation clearly Visible. Movements are Positive.',
-      advise: ''
-    }
+    Setting.first.default_obstetric_findings
   end
 end
