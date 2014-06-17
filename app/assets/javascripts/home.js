8af327337bfcefb09dbcb5b63ae0f8f1ae3f2e02
Abhishek Srivastava <abhishek77in@gmail.com>
2014-06-17 11:51:33 +0530

    Call print only if chrome is present

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,8 +1,17 @@
 $(function(){
+
+  var isChromium = window.chrome,
+    vendorName = window.navigator.vendor;
+  if(isChromium !== null && vendorName === "Google Inc.") {
+    isChrome = true;
+  } else {
+    isChrome = false;
+  }
+
   $('.printLink').click(function (e) {
     e.preventDefault();
     var id = $(this).data('report-id');
     var w = window.open("/reports/" + id + "/print");
-    w.print();
+    if(isChrome) { w.print(); }
   });
 });
