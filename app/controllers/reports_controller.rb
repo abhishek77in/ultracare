53aac55e87fd2fc87c854e9370011a33fb17bc0a
Abhishek Srivastava <abhishek77in@gmail.com>
2014-07-04 03:07:09 +0530

    Set xserver option to true

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -38,6 +38,7 @@ class ReportsController < ApplicationController
     @report = Report.find(params[:id])
     @reportable = @report.report_type.reportable
     render pdf: "#{@report.patient.name} - #{@report.created_at.strftime("%d %b %y")}",
+           use_xserver: true,
            layout: 'pdf.html.haml',
            margin: { bottom: 20, top: 1 },
            footer: { html: { template: 'shared/pdf/footer.pdf.haml', layout: 'layouts/footer.pdf.haml' } }
