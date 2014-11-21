95f42b9debfcb3e3348c73d97420b0ca388f2557
Abhishek Srivastava <abhishek77in@gmail.com>
2014-11-21 19:08:40 +0530

    Change app root

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-root = "/home/ultracare/apps/ultrasound/current"
+root = "/home/ultracare/apps/ultrasound"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
