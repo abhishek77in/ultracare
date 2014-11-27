54ba5dc79ee2a5548e89d5e732e4bd68995595a1
Abhishek Srivastava <abhishek77in@gmail.com>
2014-11-27 15:54:07 +0530

    Conditionally set wickedpdf config

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,3 +1,3 @@
-WickedPdf.config = {
-  :exe_path => '/usr/local/bin/wkhtmltopdf'
-}
+if (/darwin/ =~ RUBY_PLATFORM) || (/linux/ =~ RUBY_PLATFORM)
+  WickedPdf.config = { :exe_path => '/usr/local/bin/wkhtmltopdf' }
+end
