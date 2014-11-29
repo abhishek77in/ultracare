2d007c2df15310e23659d5d041648033225b3ab4
Abhishek Srivastava <abhishek77in@gmail.com>
2014-11-29 10:33:01 +0530

    Update doctor spec

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -4,5 +4,10 @@ RSpec.describe Doctor, :type => :model do
 
   describe 'validations' do
     it { should validate_presence_of :name }
+    it { should validate_uniqueness_of :name }
+  end
+
+  describe 'associations' do
+    it { should have_many :reports }
   end
 end
