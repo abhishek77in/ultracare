08fda6a2d3859dd79364153ceb866376d5d2642a
Abhishek Srivastava <abhishek77in@gmail.com>
2014-12-15 20:14:08 +0530

    Add high-padding for short reports

--- app/models/lower_abdomen_female.rb
+++ app/models/lower_abdomen_female.rb
@@ -1,6 +1,10 @@
 class LowerAbdomenFemale < ActiveRecord::Base
   has_one :report_type, as: :reportable
 
+  def print_template
+    'shared/pdf/lower_abdomen_female'
+  end
+
   def self.params
     Setting.first.default_organ_findings.select { |key, value| column_names.include?(key) }
   end
