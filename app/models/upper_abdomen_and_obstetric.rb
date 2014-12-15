4d8b8a181be099fbc7830f5c747ac49a9f35f65c
Abhishek Srivastava <abhishek77in@gmail.com>
2014-12-15 23:57:34 +0530

    Print upper_abdomen_and_obstetric report

--- app/models/upper_abdomen_and_obstetric.rb
+++ app/models/upper_abdomen_and_obstetric.rb
@@ -1,6 +1,14 @@
 class UpperAbdomenAndObstetric < ActiveRecord::Base
   include Reportable
 
+  def printable_fields
+    %w(liver gall_bladder pancreas spleen left_kidney right_kidney ureter other impression advise)
+  end
+
+  def print_template
+    'shared/pdf/upper_abdomen_and_obstetric'
+  end
+
   def self.params
     Setting.first.default_organ_findings.select { |key, value| column_names.include?(key) }
       .merge(Setting.first.default_obstetric_findings)
