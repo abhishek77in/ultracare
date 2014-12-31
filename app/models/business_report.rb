835072bc2239d83af850b27f65f13d2e703f4282
Abhishek Srivastava <abhishek77in@gmail.com>
2014-12-31 12:18:30 +0530

    Create date_converter module

--- app/models/business_report.rb
+++ app/models/business_report.rb
@@ -1,5 +1,6 @@
 class BusinessReport
   include ActiveModel::Model
+  include DateConverter
 
   attr_accessor :doctor_ids, :date_range
 
@@ -22,17 +23,5 @@ class BusinessReport
 
   def file_name
     "Business Report - #{Time.now.strftime("%d %b %y")}"
-  end
-
-  private
-  def to_range(date_range)
-    date_range = date_range.split(' to ')
-    start_date = to_date(date_range.first)
-    end_date = to_date(date_range.last) + 1
-    start_date..end_date
-  end
-
-  def to_date(date_string)
-    Date.strptime(date_string, '%d-%m-%Y')
   end
 end
