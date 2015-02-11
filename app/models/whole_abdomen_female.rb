6cda2de67142365432de8c2737376116dce995ee
Abhishek Srivastava <abhishek77in@gmail.com>
2015-02-12 00:08:08 +0530

    Fix advise related issues

--- app/models/whole_abdomen_female.rb
+++ app/models/whole_abdomen_female.rb
@@ -2,7 +2,7 @@ class WholeAbdomenFemale < ActiveRecord::Base
   include Reportable
 
   def printable_fields
-    %w(liver gall_bladder pancreas spleen right_kidney left_kidney urinary_bladder uterus adnexa other advise)
+    %w(liver gall_bladder pancreas spleen right_kidney left_kidney urinary_bladder uterus adnexa other)
   end
 
   def possible_genders
