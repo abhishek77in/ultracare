4c056408b53193a08223f6ff9b2e28e6e0ffaf9b
abhishek <abhishek77in@gmail.com>
2015-05-14 18:44:31 +0530

    change wkhtmltopdf path

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,3 +1,3 @@
 if Gem.win_platform?
-  WickedPdf.config = { exe_path: ['C:', 'wkhtmltopdf', 'bin', 'wkhtmltopdf.exe'].join(File::SEPARATOR) }
+  WickedPdf.config = { exe_path: ['C:', 'Program Files (x86)', 'wkhtmltopdf', 'bin', 'wkhtmltopdf.exe'].join(File::SEPARATOR) }
 end
