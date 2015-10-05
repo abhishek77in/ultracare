b1393cd1d331129db50fb4b3feb0d5d00f825c89
Abhishek Srivastava <abhishek77in@gmail.com>
2015-10-05 18:35:19 +0530

    Use simple_format

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,5 @@
 module ApplicationHelper
 
-  def convert_to_line_break(text)
-    return "" if text.blank?
-    text.gsub(/(?:\n\r?|\r\n?)/, '<br>')
-  end
-
   def date_range_param_last_month
     start_date = to_date_string(Date.today - 30)
     end_date = to_date_string(Date.today)
