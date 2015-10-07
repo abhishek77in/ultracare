11feb2c90a4466cee91cc7cd8b30a3b2054bb570
Abhishek Srivastava <abhishek77in@gmail.com>
2015-10-07 12:18:45 +0530

    Add cost to cases

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,10 @@ User.create(email: 'test@example.com', password: 'abcd1234')
 
 today = DateTime.now
 
+def random_cost
+  [500, 600, 700, 800, 900, 1000].sample
+end
+
 10.times do
   Doctor.create(name: Faker::Name.name, degree: 'MBBS')
 end
@@ -20,13 +24,13 @@ setting.default_organ_findings = {"liver"=>"Appears NORMAL in size, shape & echo
 setting.default_obstetric_findings = {"pregnancy"=>"Single\r\nConfirmed / Viable / Intrauterine", "crl_mm"=>"", "crl_weeks"=>"", "crl_days"=>"", "bpd_mm"=>"", "bpd_weeks"=>"", "bpd_days"=>"", "fl_mm"=>"", "fl_weeks"=>"", "fl_days"=>"", "ac_mm"=>"", "ac_weeks"=>"", "ac_days"=>"", "hc_mm"=>"", "hc_weeks"=>"", "hc_days"=>"", "average_g_a"=>"29 WEEKS O5 DAYS (+- 02 WEEKS)", "presentation_and_lie"=>"VERTEX", "liquor_volume"=>"Appears ADEQUATE in amount.", "cervix_and_o_s"=>"Appears Closed.", "fetal_heart"=>"PULSATING\r\nRate by M+mode 158/bpm.", "fetal_head_movement"=>"NORMAL", "fetal_limbs_movement"=>"NORMAL", "fetal_breathing_movement"=>"NORMAL", "placenta"=>"ANTERIOR", "grade"=>"2nd", "e_d_d"=>"06-06-2014(+- 02 WEEKS)", "e_f_w"=>"1430 gms. (+-20%)", "remarks"=>"Appears Single Viable Intrauterine Fetus with Normal Cardiac & Somatic Activity. Fetal Heart Pulsation clearly Visible. Movements are Positive.", "advise"=>""}
 setting.save
 
-30.times do
+10.times do
   rand(10..60).times do
     patient = Patient.new(name: Faker::Name.name, age: Faker::Number.number(2), sex: 'F' )
     doctor = Doctor.first(rand(Doctor.count) + 1).last
     reportable_model = [UpperAbdomenAndObstetric, WholeAbdomenMale, Testis, WholeAbdomenFemale, Obstetric, KubFemale, UpperAbdomen, ThyroidGland, KubMale, Breast, LowerAbdomenFemale].sample
     reportable = reportable_model.new(reportable_model.params)
-    report = Report.new(patient: patient, doctor: doctor, updated_at: today, created_at: today)
+    report = Report.new(patient: patient, doctor: doctor, updated_at: today, created_at: today, cost: random_cost)
     report.report_type = ReportType.new(reportable: reportable)
     if report.save && report.report_type.save
       puts "Report created for patient - #{report.patient.name}, doctor - #{report.doctor.name}, Date - #{today}"
