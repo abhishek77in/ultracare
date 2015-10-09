396b84335e7b2eedbd305ebbd7de384d4e369c4f
Abhishek Srivastava <abhishek77in@gmail.com>
2015-10-09 12:39:42 +0530

    Compile assets

--- compile_app.sh
+++ compile_app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# RAILS_ENV=production bundle exec rake assets:precompile
 
+RAILS_ENV=production bundle exec rake assets:precompile
 rm -rf ~/Google\ Drive/Carepill/Ultracare/ultracare_installer/ultrasound
 cp -r ../ultrasound ~/Google\ Drive/Carepill/Ultracare/ultracare_installer/
 cp -f ../ultrasound/config/database.windows.yml ~/Google\ Drive/Carepill/Ultracare/ultracare_installer/ultrasound/config/database.yml
