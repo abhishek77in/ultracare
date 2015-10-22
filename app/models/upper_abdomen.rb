f4788c7e711fe7b9f34681044d4365a4df70773c
Abhishek Srivastava <abhishek77in@gmail.com>
2015-10-23 00:50:54 +0530

    Delete ultrasound report specific file

--- app/models/upper_abdomen.rb
+++
@@ -1,8 +0,0 @@
-class UpperAbdomen < ActiveRecord::Base
-  include Reportable
-
-  def printable_fields
-    %w(liver gall_bladder pancreas spleen right_kidney left_kidney ureter other)
-  end
-
-end
