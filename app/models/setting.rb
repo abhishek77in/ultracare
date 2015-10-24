bd47e85f639979ead408b552b89e825b93d20635
Abhishek Srivastava <abhishek77in@gmail.com>
2015-10-24 18:40:16 +0530

    Store signature

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,5 +1,5 @@
 class Setting < ActiveRecord::Base
-  store :print_settings, accessors: [:heading, :sub_heading, :address, :phone_number, :consultant_1, :consultant_2], coder: JSON
+  store :print_settings, accessors: [:heading, :sub_heading, :address, :phone_number, :consultant_1, :consultant_2, :signature], coder: JSON
 
   validates :show_max_reports, numericality: { greater_than: 0, allow_nil: true  }
   validates :show_reports_from_last_days, numericality: { greater_than: 0, allow_nil: true  }
