bb7f61e6c70850ca7dcfac264bea52b41b8ecb08
Abhishek Srivastava <abhishek77in@gmail.com>
2015-10-24 22:30:30 +0530

    Customize header

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,5 +1,5 @@
 class Setting < ActiveRecord::Base
-  store :print_settings, accessors: [:heading, :sub_heading, :address, :phone_number, :consultant_1, :consultant_2, :signature, :footer], coder: JSON
+  store :print_settings, accessors: [:header, :signature, :footer], coder: JSON
 
   validates :show_max_reports, numericality: { greater_than: 0, allow_nil: true  }
   validates :show_reports_from_last_days, numericality: { greater_than: 0, allow_nil: true  }
