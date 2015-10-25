892a86b6fa7c1a7dbd61365afb7f21998322bf02
Abhishek Srivastava <abhishek77in@gmail.com>
2015-10-25 13:23:17 +0530

    On click reset settings #WIP

--- app/assets/javascripts/settings.js
+++ app/assets/javascripts/settings.js
@@ -1,13 +1,6 @@
-$(document).foundation({
-  accordion: {
-    multi_expand: false,
-    toggleable: true
-  }
-});
-
 $(function(){
-  $('dl.accordion dd a').click(function(){
-    $(this).find('span').toggleClass('fi-arrows-in');
-    $(this).find('span').toggleClass('fi-arrows-out');
-  });
+  function setDefault(section, value) {
+    CKEDITOR.instances[section].setData(value);
+    // CKEDITOR.instances['setting_footer'].setData("hello world");
+  }
 });
