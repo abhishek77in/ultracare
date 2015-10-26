6ea8c23b0a54f84cec193d2a9ac05f10e9ec7c31
Abhishek Srivastava <abhishek77in@gmail.com>
2015-10-26 10:30:50 +0530

    Allow reset to default setting

--- app/assets/javascripts/settings.js
+++ app/assets/javascripts/settings.js
@@ -3,4 +3,11 @@ $(function(){
     CKEDITOR.instances[section].setData(value);
     // CKEDITOR.instances['setting_footer'].setData("hello world");
   }
+
+  $('.reset-to-default').click(function(e){
+    e.preventDefault();
+    var section = $(this).data('section');
+    var value = $(this).data('default-setting');
+    setDefault(section, value);
+  });
 });
--- app/models/setting.rb
+++ app/models/setting.rb
@@ -3,4 +3,31 @@ class Setting < ActiveRecord::Base
 
   validates :show_max_reports, numericality: { greater_than: 0, allow_nil: true  }
   validates :show_reports_from_last_days, numericality: { greater_than: 0, allow_nil: true  }
+
+  def default_header
+    %Q[
+      <h1 style="text-align: center;">YOUR DIAGNOSTIC CENTRE</h1>
+      <h2 style="text-align: center;">DIAGNOSTIC ULTRASOUND UNIT</h2>
+      <h3 style="text-align: center;">Opposite - Memorial Hospital, City Name&nbsp;232436</h3>
+    ]
+  end
+
+  def default_signature
+    %Q[
+      <div style="width: 70%; float: left;">
+        <p>PLEASE CO-RELATE CLINICALLY</p>
+      </div>
+      <div style="width: 30%; float: right;">
+        <p>SIGNATURE<br />
+          Dr. Jai Ram Sinha</p>
+      </div>
+    ]
+  end
+
+  def default_footer
+    %Q[
+      NOT VALID FOR MEDICOLEGAL PURPOSE <br/>
+      Kindly inform any typing mistake immediately for the correction
+    ]
+  end
 end
