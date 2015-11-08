cdac9d6d781e2920b22411c3d5906ee53054b462
Abhishek Srivastava <abhishek77in@gmail.com>
2015-11-08 15:19:25 +0530

    Fix specs

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -1,12 +1,8 @@
 require 'rails_helper'
 
-feature "show header with ultrasound unit's name on header" do
-  before do
-    @setting = FactoryGirl.create(:setting, print_settings: {heading: 'My ultrasound unit'})
-  end
-
+feature "show header with Ultracare" do
   scenario "doctor visits dashboard" do
     visit '/'
-    expect(page).to have_text("MY ULTRASOUND UNIT")
+    expect(page).to have_text("Ultracare")
   end
 end
