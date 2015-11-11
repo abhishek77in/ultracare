1c634cc84375299e5d1aaeecf5d58360d029660b
Abhishek Srivastava <abhishek77in@gmail.com>
2015-11-12 01:54:28 +0530

    Hook on select2 close event instead of change

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -20,7 +20,7 @@ $(function(){
     placeholder: 'Select a template to start creating new report'
   });
 
-  $('select.template-selector').change(function() {
+  $('select.template-selector').on("select2:close", function (event) {;
     var templateId = $(this).val();
     if (templateId) {
       var url = "/reports/new?template_id=" + templateId;
