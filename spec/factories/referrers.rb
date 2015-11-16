0196513ac3c1890fdf7711287aed7b6a56c40b9d
Abhishek Srivastava <abhishek77in@gmail.com>
2015-11-17 00:17:58 +0530

    Fix sequence

--- spec/factories/referrers.rb
+++ spec/factories/referrers.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   sequence(:referrer_name) { |n| "Jon Doe #{n}" }
 
   factory :referrer do
-    name { generate(:doctor_name) }
+    name { generate(:referrer_name) }
   end
 end
