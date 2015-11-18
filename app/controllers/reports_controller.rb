c6bcfebc7d91a7ad19168be6b37795a3b9835a37
Abhishek Srivastava <abhishek77in@gmail.com>
2015-11-19 00:38:05 +0530

    Refactor template controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,14 +1,14 @@
 class ReportsController < ApplicationController
   before_action :redirect_to_root, if: -> { setting.disable_reports? }, only: [:index]
-  before_action :load_template, only: [:new]
 
   def show
     @report = Report.find(params[:id])
   end
 
   def new
-    @report = Report.new(title: @template.report_title,
-                         content: @template.content)
+    template = Template.find(params[:template_id])
+    @report = Report.new(title: template.report_title,
+                         content: template.content)
     @report.build_patient
   end
 
@@ -62,9 +62,5 @@ class ReportsController < ApplicationController
                                    :content,
                                    :title,
                                    patient_attributes: [:name, :age, :sex, :patient_id])
-  end
-
-  def load_template
-    @template = Template.find(params[:template_id])
   end
 end
