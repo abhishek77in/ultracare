5d11e1cf080bdc2e37e3618849b87fbd3e586a10
Abhishek Srivastava <abhishek77in@gmail.com>
2015-11-19 01:35:17 +0530

    Fix typo

--- app/assets/javascripts/reports.js
+++ app/assets/javascripts/reports.js
@@ -6,8 +6,8 @@ $(function(){
 
   $('select.switch-template').change(function() {
     var templateId = $(this).val();
-    var confimation = confirm('You will loose current work in progress. Press Ok to change the template otherwise press Cancel.');
-    if (templateId && confimation) {
+    var confirmation = confirm('You will loose current work in progress. Press Ok to change the template otherwise press Cancel.');
+    if (templateId && confirmation) {
       var url = '/templates/' + templateId + '.json';
       $.ajax({
         url: url,
