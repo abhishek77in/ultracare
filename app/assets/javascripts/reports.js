3369c4b62802971a95f4af933d3bdf54c4652b3c
Abhishek Srivastava <abhishek77in@gmail.com>
2015-11-19 01:38:19 +0530

    Rearrange js

--- app/assets/javascripts/reports.js
+++ app/assets/javascripts/reports.js
@@ -1,24 +1,24 @@
 $(function(){
-  $('select.switch-template').select2({
-    allowClear: true,
-    placeholder: 'Select a template to switch'
-  });
-
-  $('select.switch-template').change(function() {
-    var templateId = $(this).val();
-    var confirmation = confirm('You will loose current work in progress. Press Ok to change the template otherwise press Cancel.');
-    if (templateId && confirmation) {
-      var url = '/templates/' + templateId + '.json';
-      $.ajax({
-        url: url,
-        type: 'get',
-        dataType: 'script',
-        complete: function(request){
-          var template = jQuery.parseJSON(request.responseText);
-          CKEDITOR.instances["report_content"].setData(template.content);
-          $('h2.report-title').text(template.report_title);
-        }
-      });
-    }
-  });
+  $('select.switch-template')
+    .select2({
+      allowClear: true,
+      placeholder: 'Select a template to switch'
+    })
+    .change(function() {
+      var templateId = $(this).val();
+      var confirmation = confirm('Warning: You will loose current work progress. Press Ok to continue with change of template otherwise press Cancel.');
+      if (templateId && confirmation) {
+        var url = '/templates/' + templateId + '.json';
+        $.ajax({
+          url: url,
+          type: 'get',
+          dataType: 'script',
+          complete: function(request){
+            var template = jQuery.parseJSON(request.responseText);
+            CKEDITOR.instances["report_content"].setData(template.content);
+            $('h2.report-title').text(template.report_title);
+          }
+        });
+      }
+    });
 });
