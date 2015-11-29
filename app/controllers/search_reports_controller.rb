f497e41622ae43e68c831ff816820367bfc85f00
Abhishek Srivastava <abhishek77in@gmail.com>
2015-11-29 12:45:09 +0530

    Fix report search

--- app/controllers/search_reports_controller.rb
+++ app/controllers/search_reports_controller.rb
@@ -10,11 +10,12 @@ class SearchReportsController < ApplicationController
 
   def index
     @search_report = SearchReport.new(search_reports_params)
-    @reports = @reports.search(@search_report.keyword)
+    @reports = @reports.search(@search_report.keyword) if @search_report.keyword.present?
     @reports = @reports.belongs_to_referrer(@search_report.referrer_id)
     @reports = @reports.date_range(@search_report.converted_date_range)
     @reports = @reports.patient_name(@search_report.patient_name)
     @reports = @reports.patient_id(@search_report.patient_id)
+    @reports = @reports.decorate
   end
 
   private
