e9022b8210152f884f4a33e29ced1fd2159e75e7
Abhishek Srivastava <abhishek77in@gmail.com>
2015-11-29 20:33:12 +0530

    Fix typo

--- app/models/template.rb
+++ app/models/template.rb
@@ -5,8 +5,8 @@ class Template < ActiveRecord::Base
   def self.to_csv(options = {})
     CSV.generate(options) do |csv|
       csv << column_names
-      all.each do |product|
-        csv << product.attributes.values_at(*column_names)
+      all.each do |template|
+        csv << template.attributes.values_at(*column_names)
       end
     end
   end
