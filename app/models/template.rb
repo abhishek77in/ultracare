b0df873b093d94c1e9061a87acba40490f9c75bc
Abhishek Srivastava <abhishek77in@gmail.com>
2015-11-29 20:45:53 +0530

    Define import export columns

--- app/models/template.rb
+++ app/models/template.rb
@@ -2,11 +2,13 @@ class Template < ActiveRecord::Base
   validates_presence_of :name, :report_title, :content
   scope :ordered, -> { order(:position) }
 
+  COLUMNS_FOR_IMPORT_EXPORT = [:name, :report_title, :content]
+
   def self.to_csv(options = {})
     CSV.generate(options) do |csv|
-      csv << column_names
+      csv << COLUMNS_FOR_IMPORT_EXPORT
       all.each do |template|
-        csv << template.attributes.values_at(*column_names)
+        csv << template.attributes.values_at(*COLUMNS_FOR_IMPORT_EXPORT.map(&:to_s))
       end
     end
   end
