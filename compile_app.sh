cff0aed6519efc8e6cc4f87d36d050bfe9817e97
Abhishek Srivastava <abhishek77in@gmail.com>
2015-11-30 11:57:13 +0530

    Rename ultrasound to ultracare

--- compile_app.sh
+++ compile_app.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 RAILS_ENV=production bundle exec rake assets:precompile
-rm -rf ~/Google\ Drive/Carepill/Ultracare/ultracare_installer/ultrasound
-cp -r ../ultrasound ~/Google\ Drive/Carepill/Ultracare/ultracare_installer/
-cp -f ../ultrasound/config/database.windows.yml ~/Google\ Drive/Carepill/Ultracare/ultracare_installer/ultrasound/config/database.yml
+rm -rf ~/Google\ Drive/Carepill/Ultracare/ultracare_installer/ultracare
+cp -r ../ultracare ~/Google\ Drive/Carepill/Ultracare/ultracare_installer/
+cp -f ../ultracare/config/database.windows.yml ~/Google\ Drive/Carepill/Ultracare/ultracare_installer/ultracare/config/database.yml
 
-rm -rf ~/Google\ Drive/Carepill/Ultracare/ultracare_installer/ultrasound/.git
-rm -rf ~/Google\ Drive/Carepill/Ultracare/ultracare_installer/ultrasound/tmp
-rm -rf ~/Google\ Drive/Carepill/Ultracare/ultracare_installer/ultrasound/log
-rm -rf ~/Google\ Drive/Carepill/Ultracare/ultracare_installer/ultrasound/spec
+rm -rf ~/Google\ Drive/Carepill/Ultracare/ultracare_installer/ultracare/.git
+rm -rf ~/Google\ Drive/Carepill/Ultracare/ultracare_installer/ultracare/tmp
+rm -rf ~/Google\ Drive/Carepill/Ultracare/ultracare_installer/ultracare/log
+rm -rf ~/Google\ Drive/Carepill/Ultracare/ultracare_installer/ultracare/spec
 
-rm -rf ../ultrasound/public/assets
+rm -rf ../ultracare/public/assets
