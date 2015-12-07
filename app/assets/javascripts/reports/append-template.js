72785a82a9349ce9b7d18e5d801c4360da8e0e74
Abhishek Srivastava <abhishek77in@gmail.com>
2015-12-07 14:10:56 +0530

    Append template

---
+++ app/assets/javascripts/reports/append-template.js
@@ -0,0 +1,24 @@
+$(function(){
+  // Append template
+  $('select.append-template')
+    .select2({
+      allowClear: true,
+      placeholder: 'Select a template to append'
+    })
+    .change(function(event) {
+      event.preventDefault();
+      var templateId = $(this).val();
+      if (templateId) {
+        var url = '/templates/' + templateId + '.json';
+        $.ajax({
+          url: url,
+          type: 'GET',
+          dataType: 'script',
+          complete: function(request){
+            var template = jQuery.parseJSON(request.responseText);
+            CKEDITOR.instances["report_content"].insertHtml('<br/>' + template.content);
+          }
+        });
+      }
+    });
+});
