70375daf4ca413c51146fef44c9a9b223a6ea813
Abhishek Srivastava <abhishek77in@gmail.com>
2016-01-31 20:42:55 +0530

    Fix dashboard spec

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 feature "show header with Ultracare" do
+  before do
+    FactoryGirl.create(:setting)
+  end
+
   scenario "referrer visits dashboard" do
     visit '/'
     expect(page).to have_text("Ultracare")
